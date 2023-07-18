3e228b8885e0d42b89c7b2fed3c02c8ca6f86924
Andrew <mullinsa428@gmail.com>
2023-07-17 23:38:35 -0400

    finished

---
+++ nostaldjango/nostaldja/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Decade(models.Model):
+    name = models.CharField(max_length=40)
+    start_year = models.IntegerField()
+    end_year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Fad(models.Model):
+    name = models.CharField(max_length=100)
+    image_url = models.TextField()
+    description = models.TextField()
+    decade = models.ForeignKey(
+        Decade, on_delete=models.CASCADE, related_name="fad")
+
+    def __str__(self):
+        return self.name
